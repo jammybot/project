b794ceb103a7993354b4c22f9caab3882e9b045a
James Coney <b9011981@hallam.shu.ac.uk>
2023-04-06 18:44:08 +0100

    added sleep to script

--- gcl/startupscript.sh
+++ gcl/startupscript.sh
@@ -1,6 +1,7 @@
 !/bin/bash
 
 sudo apt update && \
+sleep 10s && \
 sudo apt install docker docker.io -y && \
 sudo docker pull jibba/web-dvwa:project && \
 sudo docker run -d -it -p 80:80 jibba/web-dvwa:project
