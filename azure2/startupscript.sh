54202d19c71a9e18a1b5edff8fd3044fe56f9143
James Coney <b9011981@hallam.shu.ac.uk>
2023-04-07 18:59:06 +0100

    apt-get fixed everything

--- azure2/startupscript.sh
+++ azure2/startupscript.sh
@@ -1,9 +1,6 @@
 !/bin/bash
 
-sudo apt update && \
-sudo apt update && \
-sudo apt update && \
-sudo apt update && \
+sudo apt-get update && \
 sudo apt install docker docker.io -y && \
 sudo docker pull jibba/web-dvwa:project && \
 sudo docker run -d -it -p 80:80 jibba/web-dvwa:project
--- gcl/startupscript.sh
+++ gcl/startupscript.sh
@@ -1,7 +1,6 @@
 !/bin/bash
 
-sudo apt update && \
-sleep 10s && \
-sudo apt install docker docker.io -y && \
+sudo apt-get update && \
+sudo apt-get install docker docker.io -y && \
 sudo docker pull jibba/web-dvwa:project && \
 sudo docker run -d -it -p 80:80 jibba/web-dvwa:project
