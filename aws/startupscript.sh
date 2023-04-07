72be4a7226a6ad643c301c7f77a4af116adc3d6e
James Coney <b9011981@hallam.shu.ac.uk>
2023-04-07 16:31:30 +0100

    reinstated old code

--- aws/startupscript.sh
+++ aws/startupscript.sh
@@ -1,7 +1,10 @@
 !/bin/bash
 
 sudo apt update && \
-sleep 10s && \
+sudo apt update && \
+sudo apt update && \
+sudo apt update && \
+
 sudo apt install docker docker.io -y && \
 sudo docker pull jibba/web-dvwa:project && \
 sudo docker run -d -it -p 80:80 jibba/web-dvwa:project
